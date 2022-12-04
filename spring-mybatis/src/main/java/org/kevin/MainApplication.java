17b5b226a0af8a7c4dff8b66a0c30f91244239f5
kevin <kevinloveling@foxmial.com>
2022-12-04 21:49:52 +0800

    feat:add config

--- spring-mybatis/src/main/java/org/kevin/MainApplication.java
+++ spring-mybatis/src/main/java/org/kevin/MainApplication.java
@@ -1,7 +1,7 @@
 package org.kevin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kevin.config.AppConfig;
-import org.kevin.data.DataProvider;
 import org.kevin.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,12 +9,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @author Kevin Liu
  * @date 2020/6/12 4:41 下午
  */
+@Slf4j
 public class MainApplication {
+
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         UserService service = applicationContext.getBean(UserService.class);
-        service.save(DataProvider.userData());
-        System.out.println(service.findById(2L));
+        //service.save(DataProvider.userData());
+        //System.out.println(service.findById(1L));
+        log.info("find user:{}",service.findById(1L));
     }
 }
--- spring-mybatis/src/main/java/org/kevin/TestApplication.java
+++ spring-mybatis/src/main/java/org/kevin/TestApplication.java
@@ -1,9 +1,12 @@
 package org.kevin;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.kevin.dao.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,10 +15,13 @@ import java.io.IOException;
  */
 public class TestApplication {
 
+    private final static Logger log = LoggerFactory.getLogger(TestApplication.class);
+
     public static void main(String[] args) throws IOException {
         val sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatisConfig.xml"));
         val sqlSession= sqlSessionFactory.openSession();
         final UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        System.out.println(mapper.getById(1L));
+        log.info("find user is {}",mapper.getById(1L));
+        sqlSession.close();
     }
 }
